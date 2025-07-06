fe6da5b5ebb52307efeba13f243c647ce1a45b3c
zentyk <zentyk@protonmail.com>
2025-07-06 15:45:26 -0600

    Added PHP Config

---
+++ db/ej.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users(
+    id int(11) NOT NULL,
+    name varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO users (id, name) VALUES (331,'John Doe');
+INSERT INTO users (id, name) VALUES (124,'Jane Smith');
+INSERT INTO users (id, name) VALUES (321,'Alice Johnson');
+INSERT INTO users (id, name) VALUES (134,'Bob Brown');
