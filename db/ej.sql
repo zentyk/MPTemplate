3b9dc5b58774db8cfeeff125b9b10118b2a53e8b
zentyk <zentyk@protonmail.com>
2025-08-06 19:46:02 -0600

    FEAT/FORM DATA
    * Modified ej.sql
    * Added dataSources.xml for PhpStorm Compatibility
    * Modified docker-compose.yml to allow conectivity to the db.

--- db/ej.sql
+++ db/ej.sql
@@ -1,9 +1,9 @@
-CREATE TABLE users(
-    id int(11) NOT NULL,
+CREATE TABLE users (
+    id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO users (id, name) VALUES (331,'John Doe');
-INSERT INTO users (id, name) VALUES (124,'Jane Smith');
-INSERT INTO users (id, name) VALUES (321,'Alice Johnson');
-INSERT INTO users (id, name) VALUES (134,'Bob Brown');
+INSERT INTO users (name) VALUES ('John Doe');
+INSERT INTO users (name) VALUES ('Jane Smith');
+INSERT INTO users (name) VALUES ('Alice Johnson');
+INSERT INTO users (name) VALUES ('Bob Brown');
